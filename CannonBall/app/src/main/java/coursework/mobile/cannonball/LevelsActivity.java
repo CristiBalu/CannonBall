429bc32cd8b87521a442f06a28b3927b2996d299
Cristi <cristian.balu@outlook.com>
2017-02-23 20:10:26 +0000

    Created the project

---
+++ CannonBall/app/src/main/java/coursework/mobile/cannonball/LevelsActivity.java
@@ -0,0 +1,13 @@
+package coursework.mobile.cannonball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LevelsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.levels_layout);
+    }
+}
